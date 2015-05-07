c4b28a6b070127c261b825af6b78cc52142117c1
Evgeni Korsunov <5345043@gmail.com>
2015-05-07 20:25:11 +0300

    remove log

--- plugin/remote-controll-listener/remote-listener.js
+++ plugin/remote-controll-listener/remote-listener.js
@@ -179,8 +179,6 @@
       token: token
     });
 
-    //socket.emit('presentation:start', getControllsState());
-
     Reveal.addEventListener( 'slidechanged', send );
     Reveal.addEventListener( 'fragmentshown', send );
     Reveal.addEventListener( 'fragmenthidden', send );
@@ -242,8 +240,6 @@
   });
 
   socket.on('remote:setState', function (data) {
-    console.log('set state', data);
-
     if (!Object.keys(data).length) return;
     if (verifySession(data)) {
       Reveal.slide(data.indexh, data.indexv, data.indexf);
